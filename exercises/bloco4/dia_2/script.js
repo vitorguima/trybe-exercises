ef39e96123457e8034ae7880eb9933a67cd7cf5e
Vitor <vitorguima@gmail.com>
2021-03-25 16:15:23 -0300

    conclusao exercicios de aula for e  for of

--- exercises/bloco4/dia_2/script.js
+++ exercises/bloco4/dia_2/script.js
@@ -14,8 +14,26 @@
 
 // Adicione o valor "Contato" no final do array menu :
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
-menu.push('Contato');
+// menu.push('Contato');
 
-console.log(menu);
+// console.log(menu);
+
+// Aula for
+
+//  Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index])
+}
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(name)
+}
