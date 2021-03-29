e7870e2bf29b7b0f26ba237ad0662dc3746868dc
Vitor <vitorguima@gmail.com>
2021-03-29 20:38:36 -0300

    exercicio 2 pt 2

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -1,16 +1,23 @@
-// exercícios parte 2 - Funções
+// exercício 2 - pt 2
 
-function checkPalindrome(wordChecked) {
-  let palindromWord = false;
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
-  if (wordChecked[0] === wordChecked[wordChecked.length - 1] && wordChecked[1] === wordChecked[wordChecked.length - 2] && wordChecked.length % 2 != 0) {
-    palindromWord = true;
-  } else {
-    palindromWord = false;
+
+let integerArray = [2, 3, 6, 7, 10, 1];
+
+function returnHigherValue(array) {
+  let higherValue = 0;
+
+  for (let index = 0; index < array.length; index += 1) {
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
+      if (array[index2] > array[index]) {
+        higherValue = array[index2];
+      }
+    }
+
   }
-
-  return palindromWord;
+  return array.indexOf(higherValue);
 }
 
-console.log(checkPalindrome('ele'))
+console.log(returnHigherValue(integerArray));
 
