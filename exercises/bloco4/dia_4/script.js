348553ec56fdebfd5b86c576cc7bf8c338c33216
Vitor <vitorguima@gmail.com>
2021-03-29 18:15:54 -0300

    exercicio 2

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -2,7 +2,8 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
   };
 
-  console.log('Bem vinda(o), ' + info.personagem)
+  console.log(info)
 
