e57d0cbb30764ca150a4e2a464f1eac6515445f5
Vitor <vitorguima@gmail.com>
2021-03-29 20:39:55 -0300

    exercicio 3 pt 2

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -1,16 +1,16 @@
-// exercício 2 - pt 2
+// exercício 3 - pt 2
 
-// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
 
-let integerArray = [2, 3, 6, 7, 10, 1];
+let integerArray = [2, 4, 6, 7, 10, 0, -3];
 
 function returnHigherValue(array) {
   let higherValue = 0;
 
   for (let index = 0; index < array.length; index += 1) {
     for (let index2 = 0; index2 < array.length; index2 += 1) {
-      if (array[index2] > array[index]) {
+      if (array[index2] < array[index]) {
         higherValue = array[index2];
       }
     }
