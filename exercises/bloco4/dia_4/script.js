08fd08321281b566f4bc46310a20d6ffead88ed9
Vitor <vitorguima@gmail.com>
2021-03-29 20:49:30 -0300

    exercicio 4 pt 2

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -1,22 +1,22 @@
-// exercício 3 - pt 2
+// exercício 4 - pt 2
 
-// Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
 
-let integerArray = [2, 4, 6, 7, 10, 0, -3];
+let integerArray = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
 function returnHigherValue(array) {
   let higherValue = 0;
 
   for (let index = 0; index < array.length; index += 1) {
     for (let index2 = 0; index2 < array.length; index2 += 1) {
-      if (array[index2] < array[index]) {
+      if (array[index2].length > array[index].length) {
         higherValue = array[index2];
       }
     }
 
   }
-  return array.indexOf(higherValue);
+  return (higherValue);
 }
 
 console.log(returnHigherValue(integerArray));
