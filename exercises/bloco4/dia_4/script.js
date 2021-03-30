c6be23f1cbfc0566dec2b49cb898a67e26e04f1e
Vitor <vitorguima@gmail.com>
2021-03-29 22:24:49 -0300

    exercicio 5 pt 2

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -1,23 +1,39 @@
-// exercício 4 - pt 2
+// exercício 5 - pt 2
 
-// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
 
-let integerArray = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let integerArray = [2, 3, 2, 5, 8, 2, 3];
 
 function returnHigherValue(array) {
   let higherValue = 0;
 
   for (let index = 0; index < array.length; index += 1) {
+
     for (let index2 = 0; index2 < array.length; index2 += 1) {
-      if (array[index2].length > array[index].length) {
+      let possibleValueCount1 =  0;
+      let possibleValueCount2 = 0;
+      let possibleValue = 0;
+
+      if (array[index2] == array[index]) {
+        possibleValueCount1 += 1;
+        possibleValueCount2 = possibleValueCount1;
         higherValue = array[index2];
-      }
-    }
-
+        possibleValue = higherValue;
+        
+        if (possibleValueCount2  > possibleValueCount1 && possibleValue > higherValue) {
+          higherValue = possibleValue
+        }
+    } } 
+    return (higherValue);
   }
-  return (higherValue);
 }
+  console.log(returnHigherValue(integerArray));
 
-console.log(returnHigherValue(integerArray));
 
+  // se a contagem de igualdade para um número A for maior que de B, devemos manter A, caso contrário, mantemos B
+
+
+
+
+  // criar let possibleValue = 0;
