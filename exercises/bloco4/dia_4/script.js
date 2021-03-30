2debb6c542af7fb71827ad967df3d836a36de061
Vitor <vitorguima@gmail.com>
2021-03-30 00:06:28 -0300

    inicio bonus 1

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -1,14 +1,28 @@
-// exercicio 7 pt - 2
-// Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// 4.4 - exercício bônus - 1
+// Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+// Atenção! Quando você tem um número pequeno à direita de um número grande, eles devem ser somados. Exemplo: XI = 10 + 1 = 11. No entanto, se o número pequeno está à esquerda de um número maior que ele, ele deve ser subtraído. Exemplo: IX = 10 - 1 = 9.
 
-function verifyWordFinal (word, ending) {
-  let wordVerify = false;
-  if (word[word.length - 2] + word[word.length - 1] == ending) {
-    wordVerify = true;
-  } else {
-    wordVerify = false;
-  }
-  return wordVerify;
+let romanNumbers = {
+  I: 1,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 49,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
 }
 
-console.log(verifyWordFinal('trybe','be'))
+// for (let index in romanNumbers) {
+//   console.log(index, romanNumbers[index]);
+// }
+
+// IVMXVV = -4 + 1000 + 10 + 5 + 5;
+
+let romanNumber = 'IVXVVM' 
+
+console.log(romanNumbers.length)
