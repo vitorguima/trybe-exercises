5ce284948d37e1a4b184ad4e3134cca2bfdd93cb
Vitor <vitorguima@gmail.com>
2021-03-29 22:37:51 -0300

    exercicio 6 pt 2

--- exercises/bloco4/dia_4/script.js
+++ exercises/bloco4/dia_4/script.js
@@ -1,40 +1,14 @@
-// exercício 5 - pt 2
+// exercicio 6 pt - 2
+// Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
 
-// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+let number = 4;
 
-
-let integerArray = [2, 3, 2, 5, 8, 2, 3];
-
-function returnHigherValue(array) {
-  let higherValue = 0;
-
-  for (let index = 0; index < array.length; index += 1) {
-
-    for (let index2 = 0; index2 < array.length; index2 += 1) {
-      let possibleValueCount1 = 0;
-      let possibleValueCount2 = 0;
-      let possibleValue = 0;
-
-      if (array[index2] == array[index]) {
-        possibleValueCount1 += 1;
-        possibleValueCount2 = possibleValueCount1;
-        higherValue = array[index2];
-        possibleValue = higherValue;
-
-        if (possibleValueCount2 > possibleValueCount1) {
-          higherValue = possibleValue
-        }
-      }
-    }
-    return (higherValue);
+function sumArrayUntilnumber(number) {
+  let sumAllNumbers = 0;
+  for (let i = 1; i <= number; i += 1) {
+    sumAllNumbers += i;
   }
+  return sumAllNumbers;
 }
-console.log(returnHigherValue(integerArray));
 
-
-  // se a contagem de igualdade para um número A for maior que de B, devemos manter A, caso contrário, mantemos B
-
-
-
-
-  // criar let possibleValue = 0;
+console.log(sumArrayUntilnumber(number))
