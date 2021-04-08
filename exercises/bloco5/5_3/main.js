81b4f5a7da8a60d0a17fd3382a61aac7ebd15c9b
Vitor <vitorguima@gmail.com>
2021-04-07 22:45:13 -0300

    todos os exercicios do estudo diario finalizados

--- exercises/bloco5/5_3/main.js
+++ exercises/bloco5/5_3/main.js
@@ -35,6 +35,14 @@ topThreeElement.addEventListener('dblclick', function () {
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
 
+topThreeElement.addEventListener('mouseover', function() {
+    topThreeElement.style.color = 'red';
+})
+
+topThreeElement.addEventListener('mouseout', function() {
+    topThreeElement.style.color = 'white';
+})
+
 // Segue abaixo um exemplo do uso de event.target:
 
 
