16e9b1d4ac22210ab70e6b89261a79272f19fb87
Vitor <vitorguima@gmail.com>
2021-04-07 22:31:09 -0300

    feito ate exercicio 4.1

--- exercises/bloco5/5_3/main.js
+++ exercises/bloco5/5_3/main.js
@@ -4,9 +4,9 @@ const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-//  Copie esse arquivo e edite apenas ele;
-//  Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-// 1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+//  1. Copie esse arquivo e edite apenas ele;
+//  2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// 2.1 Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
 function addTechClass(elementSelected, giveClassName) {
     let checkClassTech = document.getElementsByClassName(`${giveClassName}`);
@@ -14,31 +14,35 @@ function addTechClass(elementSelected, giveClassName) {
         checkClassTech[index].classList.remove(`${giveClassName}`);
     }
     elementSelected.className = giveClassName;
-    return 
+    return
 }
+addTechClass(divDois, '.tech');
 
-addTechClass(divDois,'.tech');
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
 
+let textBox = document.querySelector('#input');
+textBox.addEventListener('keydown', function () {
+    let techClassElement = document.querySelector('.tech');
+    techClassElement.innerText = 'o meu primeiro amor foi o Javascript';
+})
 
-//  Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+// 4.Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
+// 4.1 Que tal redirecionar para seu portifólio?
+let topThreeElement = document.querySelector('#mySpotrybefy');
+topThreeElement.addEventListener('dblclick', function () {
+    window.location.href = 'https://vitorguima.github.io/';
+})
 
-
-
-//  Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 1. Que tal redirecionar para seu portifólio?
-//  Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
 
 // Segue abaixo um exemplo do uso de event.target:
 
 
 function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+    // O Event é passado como um parâmetro para a função.
+    event.target.innerText = 'Opção reiniciada';
+    // O event possui várias propriedades, porém a mais usada é o event.target,
+    // que retorna o objeto que disparou o evento.
 }
 
 divUm.addEventListener('dblclick', resetText);
