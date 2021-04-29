997468d9ed0df440701f53ee3ed6bfbd897f79ba
Vitor <vitorguima@gmail.com>
2021-04-29 11:43:42 -0300

    bonus exercicio 4 finalizado

--- exercises/bloco7/dia_3/homework9.js
+++ exercises/bloco7/dia_3/homework9.js
@@ -2,15 +2,16 @@
 const assert = require('assert');
 
 const getLargestNumber = (array) => {
-  let largestNumber;
-  for (let index = 0; index < array.length - 1; index += 1) {
-      if (array[index] > array[index + 1]) {
-          largestNumber = [array[index]];
-      }
-  }
+  let largestNumber = 0;
+  array.sort(function (a, b) { return a - b});
+
+  largestNumber = array[array.length - 1];
+
   return largestNumber;
 }
 
 const parameter = [45, 8, 2, 50, 1, 7, 99];
 const result = 99;
 
+assert.strictEqual(typeof(getLargestNumber) === 'function', true);
+assert.strictEqual(getLargestNumber(parameter), 99);
