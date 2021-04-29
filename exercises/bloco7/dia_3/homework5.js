2c4595c01948e1e2bf42c885ccc1f2a7da53b10c
Vitor <vitorguima@gmail.com>
2021-04-28 21:19:17 -0300

    exercicio 2 tdd finalizado

--- exercises/bloco7/dia_3/homework5.js
+++ exercises/bloco7/dia_3/homework5.js
@@ -2,24 +2,42 @@
 
 // 1 Escreva a função addOne para passar nos testes já implementados.
 
+// const assert = require('assert');
+
+// const addOne = (array) => {
+//   let newArray = [];
+//   for (index of array) {
+//     newArray.push(index + 1);
+//   }
+//   return newArray;
+// };
+
+// const myArray = [31, 57, 12, 5];
+// const unchanged = [31, 57, 12, 5];
+// const expected = [32, 58, 13, 6];
+// const output = addOne(myArray);
+
+
+// assert.strictEqual(typeof addOne, 'function');
+// assert.deepStrictEqual(output, expected);
+// assert.deepStrictEqual(myArray, unchanged);
+
+// 2 Escreva a função wordLengths para passar nos testes já implementados.
+
 const assert = require('assert');
 
-const addOne = (array) => {
+const wordLengths = (array) => {
   let newArray = [];
-  for (index of array) {
-    newArray.push(index + 1);
+  for (word of array) {
+    newArray.push(word.length);
   }
+
   return newArray;
-};
+}
 
-const myArray = [31, 57, 12, 5];
-const unchanged = [31, 57, 12, 5];
-const expected = [32, 58, 13, 6];
-const output = addOne(myArray);
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
 
-
-assert.strictEqual(typeof addOne, 'function');
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
 assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged);
-
-
