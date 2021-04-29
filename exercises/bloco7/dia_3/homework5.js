80228a76ab21b9d452e66d87b864e3796494fe6d
Vitor <vitorguima@gmail.com>
2021-04-28 21:30:38 -0300

    exercicio 3 tdd finalizado

--- exercises/bloco7/dia_3/homework5.js
+++ exercises/bloco7/dia_3/homework5.js
@@ -24,20 +24,42 @@
 
 // 2 Escreva a função wordLengths para passar nos testes já implementados.
 
-const assert = require('assert');
+// const assert = require('assert');
 
-const wordLengths = (array) => {
-  let newArray = [];
-  for (word of array) {
-    newArray.push(word.length);
+// const wordLengths = (array) => {
+//   let newArray = [];
+//   for (word of array) {
+//     newArray.push(word.length);
+//   }
+
+//   return newArray;
+// }
+
+// const words = ['sun', 'potato', 'roundabout', 'pizza'];
+// const expected = [3, 6, 10, 5];
+
+// assert.strictEqual(typeof wordLengths, 'function');
+// const output = wordLengths(words);
+// assert.deepStrictEqual(output, expected);
+
+// 3 Escreva a função sumAllNumbers para passar nos testes já implementados.
+
+const assert = require('assert');
+// escreva a função sumAllNumbers aqui
+
+const sumAllNumbers = (array) => {
+  let sum = 0;
+  for (number of array) {
+    sum += number;
   }
 
-  return newArray;
+  return sum;
 }
 
-const words = ['sun', 'potato', 'roundabout', 'pizza'];
-const expected = [3, 6, 10, 5];
+const numbers = [9, 23, 10, 3, 8];
+const expected = 53;
+const output = sumAllNumbers(numbers);
 
-assert.strictEqual(typeof wordLengths, 'function');
-const output = wordLengths(words);
-assert.deepStrictEqual(output, expected);
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output, expected);
+
