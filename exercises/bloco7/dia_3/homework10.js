99875e2f1287b9284e21c2a3598c30079a24497b
Vitor <vitorguima@gmail.com>
2021-04-29 14:46:07 -0300

    exercicio 5 bonus nao finalizado

---
+++ exercises/bloco7/dia_3/homework10.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const Verify = (password) => {
+  // password.length > 8;
+  // password !== null;
+  // password min 1 uppercase;
+  // password min 1 lowercase;
+  // password min 1 typeof number;
+};
+
+assert.strictEqual(typeof(Verify) === 'function', true);
+
+
+
+
+
+
