c5e2e0ff3b66e755b22c9585c100555ed4e1ec4e
Vitor <vitorguima@gmail.com>
2021-05-03 13:56:48 -0300

    exercicio 3 finalizado

--- exercises/bloco8/dia_2/exercise3.js
+++ exercises/bloco8/dia_2/exercise3.js
@@ -63,9 +63,19 @@ const books = [
   },
 ];
 
-const authorBornIn1947 = () => {
-  return books.find((book) => book.author.birthYear === 1947).author.name;
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26);
 }
 
-assert.strictEqual(authorBornIn1947(), 'Stephen King');
-
+assert.deepStrictEqual(getNamedBook(), expectedResult);
