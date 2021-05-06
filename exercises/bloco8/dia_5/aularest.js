23f1ec5f270d00b594addfb9af1353ee24a65248
Vitor <vitorguima@gmail.com>
2021-05-06 12:01:07 -0300

    object destructuring concluido

---
+++ exercises/bloco8/dia_5/aularest.js
@@ -0,0 +1,54 @@
+function quantosParams(...args) {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+}
+
+// console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+// console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+// console.log(sum(4, 7, 8, 9, 60)); // 88
+
+// object destucturing
+// const product = {
+//   name: 'Smart TV Crystal UHD',
+//   price: '1899.05',
+//   seller: 'Casas de Minas',
+// };
+
+// const { name } = product;
+// const { name, seller } = product;
+
+// console.log(name);
+// console.log(seller);
+
+const student = {
+  a: 'Maria',
+  b: 'Turma B',
+  c: 'Matematica',
+};
+
+const { a: name, b: classAssigned, c: subject } = student;
+
+const studentName = name;
+const studentClass = classAssigned;
+
+// console.log(studentName);
+
+// const student = {
+//   a: 'Maria',
+//   b: 'Turma B',
+//   c: 'Matematica',
+// };
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const printProductDetails = ({ name, price, seller }) => {
+  console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+};
+
+printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
