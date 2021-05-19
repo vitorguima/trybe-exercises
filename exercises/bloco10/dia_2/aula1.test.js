e9d3fd2d6d8c36a217a75770e7e87a1a3421fb63
Vitor <vitorguima@gmail.com>
2021-05-19 15:36:18 -0300

    testes assincronos com promises e async finalizados

--- exercises/bloco10/dia_2/aula1.test.js
+++ exercises/bloco10/dia_2/aula1.test.js
@@ -30,10 +30,24 @@ describe('Quando o tipo do animal, existe', () => {
   });
 });
 
+test('Testando com async/await', async () => {
+  const listDogs = await getListAnimals('Dog');
+  expect(listDogs[0].name).toEqual('Dorminhoco');
+  expect(listDogs[1].name).toEqual('Soneca');
+});
+
 describe('Quando o tipo do animal, não existe', () => {
   test('Retorne a lista de animais', () => {
     return getListAnimals('Lion').catch(error =>
       expect(error).toEqual({ error: "Não possui esse tipo de animal." })
     );
   });
+});
+
+test('Testando com async/await, testando o reject', async () => {
+  try {
+    await getListAnimals('Lion');
+  } catch (error) {
+    expect(error).toEqual({ error: "Não possui esse tipo de animal." })
+  }
 });
