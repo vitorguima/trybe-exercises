05efee5cadc0f117fa9f1dbbddcee401e203381d
Vitor <vitorguima@gmail.com>
2021-05-20 14:04:39 -0300

    refactor ex 2 pt. 2

--- exercises/bloco10/dia_1/techList.js
+++ exercises/bloco10/dia_1/techList.js
@@ -1,12 +1,5 @@
 function techList(array, nome) {
-  if (array.length === 0) {
-    return 'Vazio!';
-  }
-
-  return array.sort().map((element) => ({ 
-    tech: element,
-    name: nome,
-  }))
+  return array.length === 0 ? 'Vazio!' : array.sort().map((element) => ({ tech: element, name: nome, }))
 }
 
 module.exports = techList;
