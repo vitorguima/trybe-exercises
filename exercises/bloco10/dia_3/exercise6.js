d221793472665de066ace901e3e774facfd2edea
Vitor <vitorguima@gmail.com>
2021-05-21 10:40:30 -0300

    ex 6 finalizado

--- exercises/bloco10/dia_3/exercise6.js
+++ exercises/bloco10/dia_3/exercise6.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 function fetchDogs() {
   return fetch('https://dog.ceo/api/breeds/image/random')
     .then((response) => response.json())
-    .then((json) => console.log(json))
+    .then((json) => (json))
     .catch((error) => 'request failed')
 }
 
--- exercises/bloco10/dia_3/exercise6.test.js
+++ exercises/bloco10/dia_3/exercise6.test.js
@@ -3,14 +3,17 @@ const getDogs = require('./exercise6.js');
 describe('testando a requisicao realizada pela função fetchDogs', () => {
   test('mock para a requisicao resolvida', async () => {
     getDogs.fetchDogs = jest.fn()
-    .mockResolvedValueOnce('request success')
-    .mockRejectedValueOnce('request failed')
+    .mockResolvedValue('request success');
+  
+    expect(getDogs.fetchDogs()).resolves.toBe('request success');
+    expect(getDogs.fetchDogs).toHaveBeenCalledTimes(1);
+  })
 
-    await getDogs.fetchDogs();
-    expect(getDogs.fetchDogs()).toEqual('request success');
-    expect(getDogs.fetchDogs()).toHaveBeenCalledTimes(1);
-    await getDogs.fetchDogs();
-    expect(getDogs.fetchDogs()).toEqual('request failed');
-    expect(getDogs.fetchDogs()).toHaveBeenCalledTimes(2);
+  test('mock para a requisicao resolvida', async () => {
+    getDogs.fetchDogs.mockReset;
+    getDogs.fetchDogs.mockRejectedValue('request failed');
+
+    expect(getDogs.fetchDogs()).rejects.toBe('request failed')
+    expect(getDogs.fetchDogs).toHaveBeenCalledTimes(2);
   })
 })
