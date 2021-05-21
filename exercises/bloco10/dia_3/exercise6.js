bd85b8184b8ce7af87325f0ef889282f9c9b3e7d
Vitor <vitorguima@gmail.com>
2021-05-21 14:50:01 -0300

    ajuste exercicios

--- exercises/bloco10/dia_3/exercise6.js
+++ exercises/bloco10/dia_3/exercise6.js
@@ -3,10 +3,9 @@ const fetch = require('node-fetch');
 function fetchDogs() {
   return fetch('https://dog.ceo/api/breeds/image/random')
     .then((response) => response.json())
-    .then((json) => (json))
-    .catch((error) => 'request failed')
+    .then((json) => console.log(json))
+    .catch((error) => console.log('failed'))
 }
-
 
 
 module.exports = { fetchDogs };
