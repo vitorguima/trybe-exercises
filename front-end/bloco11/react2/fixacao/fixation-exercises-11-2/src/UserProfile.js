21c5f554118368922329b83ee54864b89e8248fc
Vitor <vitorguima@gmail.com>
2021-05-25 12:07:42 -0300

    exercicios fixacao finalizados ate lista de componentes e chaves

--- front-end/bloco11/react2/fixacao/fixation-exercises-11-2/src/UserProfile.js
+++ front-end/bloco11/react2/fixacao/fixation-exercises-11-2/src/UserProfile.js
@@ -5,7 +5,7 @@ import Image from './Image';
 class UserProfile extends React.Component {
   render() {
     return (
-      <div>
+      <div >
         <p> {this.props.user.name} </p>
         <p> {this.props.user.email} </p>
         <Image source={this.props.user.avatar} alternativeText="User avatar" />
