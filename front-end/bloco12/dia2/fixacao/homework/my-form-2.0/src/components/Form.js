a81697d2cbd5dbf4c257ef6054d28cf57b29673a
Vitor <vitorguima@gmail.com>
2021-06-30 15:23:19 -0300

    primeiras mudancas

--- front-end/bloco12/dia2/fixacao/homework/my-form-2.0/src/components/Form.js
+++ front-end/bloco12/dia2/fixacao/homework/my-form-2.0/src/components/Form.js
@@ -2,34 +2,7 @@ import React, { Component } from 'react'
 import FormSignUp from './FormSignUp';
 import RenderFields from './RenderFields';
 
-const INITIAL_STATE = {
-  name: '',
-  email: '',
-  cpf: '',
-  address: '',
-  city: '',
-  state: '',
-  home: '',
-  resume: '',
-  job: '',
-  enter: 'false',
-  description: '',
-  submition: 'false',
-}
-
 export default class Form extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = INITIAL_STATE;
-    this.handlerChanger = this.handlerChanger.bind(this);
-    this.updateState = this.updateState.bind(this);
-    this.handlerBlur = this.handlerBlur.bind(this);
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handlerSubmit = this.handlerSubmit.bind(this);
-    this.handlerClear = this.handlerClear.bind(this);
-  }
-
   updateState(name, value) {
 
     this.setState(() => ({
--- front-end/bloco12/dia2/fixacao/homework/my-form-2.0/src/index.js
+++ front-end/bloco12/dia2/fixacao/homework/my-form-2.0/src/index.js
@@ -1,12 +1,14 @@
+import { Provider } from 'react-redux';
+import store from './store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
